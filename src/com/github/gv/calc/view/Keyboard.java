b4f103151c5a3c7673615a1fb4b9a8782719c7ad
gianverdum <gian.verdum@gmail.com>
2025-04-16 18:21:24 -0300

    Creating buttons

--- src/com/github/gv/calc/view/Keyboard.java
+++ src/com/github/gv/calc/view/Keyboard.java
@@ -11,5 +11,48 @@ public class Keyboard extends JPanel {
 
     public Keyboard() {
 
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        setLayout(layout);
+
+        // Row 1
+        addButton("AC", COLOR_DARK_GRAY, constraints, 0, 0);
+        addButton("+/-", COLOR_DARK_GRAY, constraints, 1, 0);
+        addButton("%", COLOR_DARK_GRAY, constraints, 2, 0);
+        addButton("/", COLOR_ORANGE, constraints, 3, 0);
+
+        // Row 2
+        addButton("7", COLOR_LIGHT_GRAY, constraints, 0, 1);
+        addButton("8", COLOR_LIGHT_GRAY, constraints, 1, 1);
+        addButton("9", COLOR_LIGHT_GRAY, constraints, 2, 1);
+        addButton("*", COLOR_ORANGE, constraints, 3, 1);
+
+        // Row 3
+        addButton("4", COLOR_LIGHT_GRAY, constraints, 0, 2);
+        addButton("5", COLOR_LIGHT_GRAY, constraints, 1, 2);
+        addButton("6", COLOR_LIGHT_GRAY, constraints, 2, 2);
+        addButton("-", COLOR_ORANGE, constraints, 3, 2);
+
+        // Row 4
+        addButton("1", COLOR_LIGHT_GRAY, constraints, 0, 3);
+        addButton("2", COLOR_LIGHT_GRAY, constraints, 1, 3);
+        addButton("3", COLOR_LIGHT_GRAY, constraints, 2, 3);
+        addButton("+", COLOR_ORANGE, constraints, 3, 3);
+
+        // Row 5
+        addButton("0", COLOR_LIGHT_GRAY, constraints, 0, 4);
+        addButton("0", COLOR_LIGHT_GRAY, constraints, 1, 4);
+        addButton(",", COLOR_LIGHT_GRAY, constraints, 2, 4);
+        addButton("=", COLOR_ORANGE, constraints, 3, 4);
+
+    }
+
+    private void addButton(String text, Color color, GridBagConstraints constraints, int x, int y) {
+
+        constraints.gridx = x;
+        constraints.gridy = y;
+        Button button = new Button(text, color);
+        add(button, constraints);
     }
 }
