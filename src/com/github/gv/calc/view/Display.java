6f9d616c8717b1615c3f13888178bcd4a332c030
gianverdum <gian.verdum@gmail.com>
2025-04-16 17:27:14 -0300

    Created display

--- src/com/github/gv/calc/view/Display.java
+++ src/com/github/gv/calc/view/Display.java
@@ -5,7 +5,16 @@ import java.awt.*;
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+
     public Display() {
-        setBackground(Color.GREEN);
+        setBackground(new Color(46,49,50));
+        label = new JLabel("1234,56");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Courier", Font.PLAIN, 20));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT,10,25));
+        add(label);
+
     }
 }
--- src/com/github/gv/calc/view/Keyboard.java
+++ src/com/github/gv/calc/view/Keyboard.java
@@ -1,11 +1,10 @@
 package com.github.gv.calc.view;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Keyboard extends JPanel {
 
     public Keyboard() {
-        setBackground(Color.RED);
+
     }
 }
